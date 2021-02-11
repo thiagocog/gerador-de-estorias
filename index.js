6eabc92e804a4cda47cdf2232217f80e27c3538b
Thiago Carvalho de Onofre Gonçalves <thiago.cgoncalves@al.infnet.edu.br>
2021-02-11 22:47:59 +0000

    Iniciados os templates index e about

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 // IMPORTANDO PACOTES
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
 
 
 // CRIANDO A APLICAÇÃO
-const app = express();
+const app = express()
 
 
 // FAZENDO A LIGAÇÃO COM OS CONTROLLERS DAS ROTAS
-const aboutController = require('./controller/about-controller');
-const formController = require('./controller/form-controller');
+const aboutController = require('./controller/about-controller')
+const formController = require('./controller/form-controller')
 
 
 // ESTABELECENDO QUE A VIEW ENGINE UTILIZADA SERÁ O EJS
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 
 // ESTABELECENDO QUAIS ARQUIVOS ESTÁTICOS SERÃO UTILIZADOS
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 // CONFIGURANDO O BODY-PARSER
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 
 // CONFIGURANDO O HANDLE RAIZ (HOME OU INDEX)
 const handleHome = (req, res, next) => {
-  res.render('index');
+  res.render('index')
   // next();
-};
+}
 
 
 // CONFIGURANDO AS ROTAS DA APLICAÇÃO
 app.get('/', handleHome);
-app.get('/form', formController.getForm);
-app.post('/form', formController.postForm);
-app.get('/about', aboutController.getAbout);
+app.get('/form', formController.getForm)
+app.post('/form', formController.postForm)
+app.get('/about', aboutController.getAbout)
 
 
 // ABRINDO O SERVIDOR NA PORTA 3001
 app.listen(3001, () => {
-  console.log('O servidor está rodando.');
-});
+  console.log('Server running at http://localhost:3001')
+})
